d777182808c7ef0e5d6bd7725013b1a3730c7e1d
ColtJLaCh <CL51@myscc.ca>
2022-02-20 15:20:00 -0500

    Add functionality to hover-affect attr directive

--- C_LaChance_MyVideoGameProjects/src/app/hover-affect.directive.ts
+++ C_LaChance_MyVideoGameProjects/src/app/hover-affect.directive.ts
@@ -1,10 +1,40 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appHoverAffect]'
 })
 export class HoverAffectDirective {
 
-  constructor() { }
+  @Input() textDecor?: string;
+  constructor(private elm: ElementRef) { }
+
+  @HostListener('mouseenter') onMouseEnter() {
+      this.styleMe(true);
+  }
+
+  @HostListener('mouseleave') onMouseExit() {
+    this.styleMe(false);
+  }
+
+  //I'll optomize this in the future with a switch and multiple methods if need be
+  private styleMe(isHovering: boolean) {
+    if (this.textDecor == "bold") {
+      if (isHovering) {
+        this.elm.nativeElement.style.fontWeight = this.textDecor;
+      } else {
+        this.elm.nativeElement.style.fontWeight = "normal";
+      }
+    } else {
+      if (isHovering) {
+        this.elm.nativeElement.style.textDecoration = this.textDecor;
+      } else {
+        this.elm.nativeElement.style.textDecoration = "none";
+      }
+    }
+  }
+
+  ngOnInit(): void {
+    this.textDecor = (this.textDecor == null ? "underline" : this.textDecor);
+  }
 
 }
